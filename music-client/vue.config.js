1113a25a8d0ff593631d5d19fc06579f453489d7
贺英达 <124332948+threeFeetCat123@users.noreply.github.com>
2024-03-22 14:59:21 +0800

    Add files via upload

--- music-client/vue.config.js
+++ music-client/vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+  },
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
       Object.assign(definitions[0]['process.env'], {
